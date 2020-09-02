1ba2041c0de8188826d7e38fdcddac2a6ab315f3
Ezra Tsai <ishow8166@icloud.com>
2020-09-02 16:46:08 +0800

    add detail page

--- app.js
+++ app.js
@@ -18,12 +18,26 @@ app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     return Todo.findAll({
-      raw: true,
-      nest: true
+        raw: true,
+        nest: true
     })
-      .then((todos) => { return res.render('index', { todos: todos }) })
-      .catch((error) => { return res.status(422).json(error) })
-  })
+        .then((todos) => { return res.render('index', { todos: todos }) })
+        .catch((error) => { return res.status(422).json(error) })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
+})
 
 app.get('/users/login', (req, res) => {
     res.render('login')
